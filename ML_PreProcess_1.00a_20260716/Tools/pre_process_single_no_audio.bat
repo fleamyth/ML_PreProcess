6ba34022e06fa18527863049343a1dda2c2a812c
Flea Wu (PEGATRON CORPORATION) <v-fleawu@microsoft.com>
2026-09-01 17:45:36 +0800

    update preprocessing error mappings and audio handling

--- ML_PreProcess_1.00a_20260716/Tools/pre_process_single_no_audio.bat
+++ ML_PreProcess_1.00a_20260716/Tools/pre_process_single_no_audio.bat
@@ -1,3 +1,3 @@
-pre_process.exe --debug --skip_audio=True --config=robocalv4_config_pre_post.yaml %1 %2 %3
+pre_process.exe --debug --skip_audio=False --config=robocalv4_config_pre_post.yaml %1 %2 %3
 echo %errorlevel%
 exit /b %errorlevel%
